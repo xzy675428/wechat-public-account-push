7c3dcbc567cdb88940cd2ad97b86872fbddf5bdd
xzy675428 <119779080+xzy675428@users.noreply.github.com>
2022-12-04 22:26:04 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -5,7 +5,7 @@ export const config = {
     // 公众号appSecret
     appSecret: "8513ed993ffa8be5f7527558ef6b0967",
     // 模板消息id
-    templateId: "Lbs9iy8jt038U2zuE62flS2SmuVBZv73ccuMCLstOqs",
+    templateId: "V62josKKiGqvKlCZE0RKoubdkkeg9g-ejNhUQD61tRw",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
     user: ["omHDt6vBNBq0ko7Jmg6rKLRIwsYU"],
      
@@ -17,7 +17,7 @@ export const config = {
     city: "上海",
     // 生日，修改名字为对应需要显示的名字，如果生日为农历， type为 r
     birthdays: [
-      {"name": "小宝", "year": "2004", "date": "02-15", "type": 'new'},
+      {"name": "小俞", "year": "2004", "date": "02-15", "type": 'new'},
       {"name": "小羊", "year": "2004", "date": "02-10", "r": 'new'},
     ],
     // 在一起的日子，格式同上
@@ -32,7 +32,6 @@ export const config = {
 //最低气温: {{min_temperature.DATA}}  
 //最高气温: {{max_temperature.DATA}}  
 //今天是我们恋爱的第{{love_day.DATA}}天
-//距离小羊的生日还有{{birthday_message.DATA}}天
 //距离小俞的生日还有{{birthday_message.DATA}}天
 
 // {{note_en.DATA}}  
